746f14d93fd681b944fb233503947dcd1f1d1513
Strocs <strocsdev@gmail.com>
2025-12-26 15:29:44 -0300

    feat(config): add remote image patterns for CDN in next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.frijolmagico.cl',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
