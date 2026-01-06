e36960e8406ddbec1073b5fec015d4c33fe6002f
Strocs <strocsdev@gmail.com>
2026-01-05 23:43:51 -0300

    chore(db): update seed.sql for new schema

--- db/seed/seed.sql
+++ db/seed/seed.sql
@@ -13,3 +13,12 @@ INSERT OR IGNORE INTO disciplina (nombre) VALUES ('Ilustración');
 INSERT OR IGNORE INTO disciplina (nombre) VALUES ('Narrativa gráfica');
 INSERT OR IGNORE INTO disciplina (nombre) VALUES ('Manualidades');
 INSERT OR IGNORE INTO disciplina (nombre) VALUES ('Fotografía');
+
+-- =============================================================================
+-- ARTISTA ESTADOS
+-- =============================================================================
+
+INSERT OR IGNORE INTO artista_estado (estado) VALUES ('desconocido');
+INSERT OR IGNORE INTO artista_estado (estado) VALUES ('activo');
+INSERT OR IGNORE INTO artista_estado (estado) VALUES ('inactivo');
+INSERT OR IGNORE INTO artista_estado (estado) VALUES ('cancelado');
