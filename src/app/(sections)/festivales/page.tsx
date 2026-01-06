0abd5e203d7958b7e5e15486adae568abb6c8766
Strocs <strocsdev@gmail.com>
2026-01-06 17:40:33 -0300

    chore(festivales): update festival page for minor content or data sync

--- src/app/(sections)/festivales/page.tsx
+++ src/app/(sections)/festivales/page.tsx
@@ -2,5 +2,7 @@ import { redirect } from 'next/navigation'
 import { paths } from '@/config/paths'
 
 export default function FestivalesPage() {
+  // TODO: Página con línea de tiempo de los festivales pasados.
+  // consultar en base de datos por datos de métricas (temporalmente)
   redirect(paths.festival[2025].base)
 }
