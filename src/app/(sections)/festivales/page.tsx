7e32cc9fd2650e8ced63e8ecffda4267319dd045
Strocs <strocsdev@gmail.com>
2026-01-10 20:59:23 -0300

    chore(festivales): update main festivales page for new structure

--- src/app/(sections)/festivales/page.tsx
+++ src/app/(sections)/festivales/page.tsx
@@ -1,8 +1,36 @@
-import { redirect } from 'next/navigation'
-import { paths } from '@/config/paths'
+import { Header } from '@/components/Header'
+import { ErrorSection } from '@/components/ErrorSection'
+import siteData from '@/data/site.json'
+import { getFestivalesData } from './lib/getFestivalesData'
+import { FestivalesTimeline } from './components/FestivalesTimeline'
+import { Footer } from '@/components/Footer'
+import { TrackPageView } from '@/components/analytics/TrackPageView'
+import { ViewTransition } from 'react'
+import { SectionHomeButton } from '@/components/SectionsHomeButton'
 
-export default function FestivalesPage() {
-  // TODO: Página con línea de tiempo de los festivales pasados.
-  // consultar en base de datos por datos de métricas (temporalmente)
-  redirect(paths.festival[2025].base)
+const festivals = siteData.festivals
+
+export default async function FestivalesPage() {
+  const { data: festivales, error } = await getFestivalesData()
+
+  return (
+    <>
+      <TrackPageView sectionName='Festivales' sectionPath='/festivales' />
+      <ViewTransition name='transition-logo'>
+        <SectionHomeButton />
+      </ViewTransition>
+      <Header
+        title={festivals.title}
+        subTitle={festivals.subtitle}
+        description={festivals.description}
+      />
+
+      {error ? (
+        <ErrorSection error={error.message} />
+      ) : (
+        <FestivalesTimeline festivales={festivales} />
+      )}
+      <Footer />
+    </>
+  )
 }
