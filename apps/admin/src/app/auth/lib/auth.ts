27c982f959ae08d2840a844745244d477cbd06fd
Strocs <strocsdev@gmail.com>
2026-02-03 11:08:25 -0300

    fix(auth): trustedOrigins values now returns fallback value to avoid undefined

--- apps/admin/src/app/auth/lib/auth.ts
+++ apps/admin/src/app/auth/lib/auth.ts
@@ -37,6 +37,6 @@ export const auth = betterAuth({
       }
     })
   },
-  trustedOrigins: [process.env.BETTER_AUTH_URL],
+  trustedOrigins: [process.env.BETTER_AUTH_URL || 'http://localhost:3001'],
   plugins: [nextCookies()]
 })
