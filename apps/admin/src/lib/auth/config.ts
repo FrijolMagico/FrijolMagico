491300647ea8f6d27439b9aa94995d71e5f3734f
Strocs <strocsdev@gmail.com>
2026-02-11 21:20:18 -0300

    feat(admin): add auth helpers, cdn and navigation utilities

---
+++ apps/admin/src/lib/auth/config.ts
@@ -0,0 +1,3 @@
+export const ALLOWED_DOMAIN = '@frijolmagico.cl'
+export const SESSION_EXPIRATION_TIME = 60 * 60 * 24 * 3 // 3 days
+export const SESSION_UPDATE_AGE = 60 * 60 * 24 // 24 hours
---
+++ apps/admin/src/lib/navigation.ts
@@ -0,0 +1,57 @@
+import {
+  Building2,
+  Calendar,
+  Home,
+  Palette,
+  Settings,
+  Users
+} from 'lucide-react'
+
+export const navigation = [
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+    icon: Home
+  },
+  {
+    title: 'General',
+    href: '/general',
+    icon: Building2
+  },
+  {
+    title: 'Catálogo',
+    icon: Palette,
+    href: '/catalogo',
+    items: [
+      {
+        title: 'Artistas',
+        href: '/catalogo/artistas'
+      }
+    ]
+  },
+  {
+    title: 'Eventos',
+    icon: Calendar,
+    href: '/eventos',
+    items: [
+      {
+        title: 'Festivales',
+        href: '/eventos/festivales'
+      },
+      {
+        title: 'Ediciones',
+        href: '/eventos/ediciones'
+      }
+    ]
+  },
+  {
+    title: 'Usuarios',
+    href: '/usuarios',
+    icon: Users
+  },
+  {
+    title: 'Configuración',
+    href: '/configuracion',
+    icon: Settings
+  }
+]
