018b1f37a5fae5469a46a084ae82726b1868eb9c
Strocs <strocsdev@gmail.com>
2026-02-11 21:18:39 -0300

    ci(playwright): update admin playwright config

--- apps/admin/playwright.config.ts
+++ apps/admin/playwright.config.ts
@@ -1,22 +1,26 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './__tests__/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { outputFolder: './__tests__/playwright-report' }]],
+  outputDir: './__tests__/test-results',
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3001',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure'
   },
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
     }
   ],
 
