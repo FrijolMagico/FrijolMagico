dc96c4a3152488953f8dbf132301df91e0550c4a
Strocs <strocsdev@gmail.com>
2026-02-22 12:57:21 -0300

    refactor(get-artists): add artists status into artists metadata

--- apps/admin/src/app/(authenticated)/(admin)/artistas/_lib/get-artists-data.ts
+++ apps/admin/src/app/(authenticated)/(admin)/artistas/_lib/get-artists-data.ts
@@ -4,27 +4,32 @@ import { cacheTag } from 'next/cache'
 
 import { db } from '@frijolmagico/database/orm'
 import { artist } from '@frijolmagico/database/schema'
-import { asc } from 'drizzle-orm'
+import { asc, eq } from 'drizzle-orm'
 
 import type { ArtistEntry } from '../_types'
 import { ARTISTA_CACHE_TAG } from '../_constants'
 
-const { artista } = artist
+const { artista, artistaEstado } = artist
 
 export async function getArtists(): Promise<ArtistEntry[] | null> {
   'use cache'
   cacheTag(ARTISTA_CACHE_TAG)
 
   const results = await db
-    .select()
+    .select({
+      artista: artista,
+      estadoSlug: artistaEstado.slug
+    })
     .from(artista)
+    .leftJoin(artistaEstado, eq(artista.estadoId, artistaEstado.id))
     .orderBy(asc(artista.pseudonimo))
 
-  if (results === undefined) return null
+  if (results === undefined || results.length === 0) return null
 
   return results.map((row) => ({
-    ...row,
-    id: String(row.id),
-    rrss: row.rrss ? JSON.parse(row.rrss) : null
+    ...row.artista,
+    estadoSlug: row.estadoSlug ?? 'desconocido',
+    id: String(row.artista.id),
+    rrss: row.artista.rrss ? JSON.parse(row.artista.rrss) : null
   }))
 }
