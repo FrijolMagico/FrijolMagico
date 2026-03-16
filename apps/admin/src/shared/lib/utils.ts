31ebf628427314c79453b16ff5aeff2a2172d656
Strocs <strocsdev@gmail.com>
2026-03-16 15:28:04 -0300

    refactor: migrate lib utilities to shared module

--- apps/admin/src/shared/lib/utils.ts
+++ apps/admin/src/shared/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function toSlug(str: string): string {
   return str
     .normalize('NFD')
