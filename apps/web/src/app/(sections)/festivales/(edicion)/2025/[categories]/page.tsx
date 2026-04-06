f7c22e3a073c3eeafce1cfa26e803202d97e9f28
Strocs <strocsdev@gmail.com>
2026-04-06 16:32:21 -0400

    refactor(convocatoria,festivales): redirect to home - 2025 edition content paused

--- apps/web/src/app/(sections)/festivales/(edicion)/2025/[categories]/page.tsx
+++ apps/web/src/app/(sections)/festivales/(edicion)/2025/[categories]/page.tsx
@@ -1,61 +1,65 @@
 import { Metadata } from 'next'
-import { ViewTransition } from 'react'
 import siteData from '@/data/site.json'
-import { ErrorSection } from '@/components/ErrorSection'
 import { normalizeString } from '@frijolmagico/utils/string'
-import { ApprovedArtistsPresentation } from './components/ApprovedArtistsPresentation'
-import { ApprovedArtistsCategoriesNav } from './components/ApprovedArtistsCategoriesNav'
-import { getApprovedArtistsData } from './lib/getApprovedArtistsData'
-import { SectionHomeButton } from '@/components/SectionsHomeButton'
-import { FestivalHeader } from '../components/FestivalHeader'
-import { TrackPageView } from '@/components/analytics/TrackPageView'
+// import { ViewTransition } from 'react'
+// import { ErrorSection } from '@/components/ErrorSection'
+// import { ApprovedArtistsPresentation } from './components/ApprovedArtistsPresentation'
+// import { ApprovedArtistsCategoriesNav } from './components/ApprovedArtistsCategoriesNav'
+// import { getApprovedArtistsData } from './lib/getApprovedArtistsData'
+// import { SectionHomeButton } from '@/components/SectionsHomeButton'
+// import { FestivalHeader } from '../components/FestivalHeader'
+// import { TrackPageView } from '@/components/analytics/TrackPageView'
+import { redirect } from 'next/navigation'
 
 type CategoryParams = {
   categories: keyof typeof siteData.selected_artists.seo.category
 }
 
-export default async function ApprovedArtistsPage({
-  params
-}: {
-  params: Promise<CategoryParams>
-}) {
-  const { data, error } = await getApprovedArtistsData()
-
-  const groupedArtists = Object.groupBy(data || [], ({ category }) =>
-    normalizeString(category)
-  )
-
-  const { categories } = await params
-  const artistsByCategory = groupedArtists[categories]
-
-  return (
-    <>
-      <TrackPageView
-        sectionName={`Festival 2025 - ${categories}`}
-        sectionPath={`/festivales/2025/${categories}`}
-      />
-      <FestivalHeader
-        title={siteData.selected_artists.title}
-        subTitle={siteData.selected_artists.subtitle}
-      />
-      <main className={`relative container mx-auto h-full pt-8`}>
-        <ViewTransition name='transition-logo'>
-          <SectionHomeButton />
-        </ViewTransition>
-        {error ? (
-          <ErrorSection error={error.message} />
-        ) : (
-          <>
-            <ApprovedArtistsCategoriesNav currentCategory={categories} />
-            <ApprovedArtistsPresentation
-              artists={artistsByCategory || []}
-              key={categories}
-            />
-          </>
-        )}
-      </main>
-    </>
-  )
+export default async function ApprovedArtistsPage(
+  {
+    // params
+  }: {
+    params: Promise<CategoryParams>
+  }
+) {
+  redirect('/')
+  // const { data, error } = await getApprovedArtistsData()
+  //
+  // const groupedArtists = Object.groupBy(data || [], ({ category }) =>
+  //   normalizeString(category)
+  // )
+  //
+  // const { categories } = await params
+  // const artistsByCategory = groupedArtists[categories]
+  //
+  // return (
+  //   <>
+  //     <TrackPageView
+  //       sectionName={`Festival 2025 - ${categories}`}
+  //       sectionPath={`/festivales/2025/${categories}`}
+  //     />
+  //     <FestivalHeader
+  //       title={siteData.selected_artists.title}
+  //       subTitle={siteData.selected_artists.subtitle}
+  //     />
+  //     <main className={`relative container mx-auto h-full pt-8`}>
+  //       <ViewTransition name='transition-logo'>
+  //         <SectionHomeButton />
+  //       </ViewTransition>
+  //       {error ? (
+  //         <ErrorSection error={error.message} />
+  //       ) : (
+  //         <>
+  //           <ApprovedArtistsCategoriesNav currentCategory={categories} />
+  //           <ApprovedArtistsPresentation
+  //             artists={artistsByCategory || []}
+  //             key={categories}
+  //           />
+  //         </>
+  //       )}
+  //     </main>
+  //   </>
+  // )
 }
 
 export async function generateMetadata({
