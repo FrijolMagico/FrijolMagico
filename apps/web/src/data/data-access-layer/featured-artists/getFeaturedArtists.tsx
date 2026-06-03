5b38ef5a3f4df1d8558434c3ba5224b7e6ceb24d
Ignacio Molina Palominos <strocsdev@gmail.com>
2026-06-02 23:03:23 -0400

    test(web): remove use cache from getFeaturedArtists to diagnose revalidation issue

--- apps/web/src/data/data-access-layer/featured-artists/getFeaturedArtists.tsx
+++ apps/web/src/data/data-access-layer/featured-artists/getFeaturedArtists.tsx
@@ -1,12 +1,8 @@
 import { FeaturedArtist } from '@/types/artists'
 import { executeQuery } from '@frijolmagico/database/client'
 import { loadSql } from '@frijolmagico/database/sql'
-import { cacheTag } from 'next/cache'
 
 export const getFeaturedArtists = async () => {
-  'use cache'
-  cacheTag('home:featured-artists')
-
   const query = loadSql(import.meta.url, './featured-artists.sql')
   try {
     const { data, error } = await executeQuery<FeaturedArtist>(query, [])
